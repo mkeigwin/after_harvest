5a3c6910ed216ff0799c13d3f44aa712a2f54bd0
matt keigwin <mkeigwin@bates.edu>
2018-02-28 14:45:11 -0500

    begin javascript

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-console.log('i am alive!')
+
+const ciders = document.querySelectorAll('.ciders')
+const test = document.querySelectorAll('.test')
+
+function selectcider(e) {
+  test.forEach(test => test.style.visibility = 'hidden')
+  const childciders = e.target.childNodes
+  childciders.forEach(cider => cider.style.visibility = 'visible')
+  if (((window.innerWidth)/2) > e.target.offsetLeft) {
+    childciders.forEach(cider => {
+      console.log(cider)
+      cider.style.left = 450
+      console.log(cider)
+    })
+  } else {
+    childciders.forEach(cider => {
+      cider.style.left = 0
+      console.log(cider.style.left)
+    })
+  }
+}
+ciders.forEach(cider => cider.addEventListener('click', selectcider))
+
 })
