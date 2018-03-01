ed93f209eddd78b65a68db4a830b7ec358f1fe3e
matt keigwin <mkeigwin@bates.edu>
2018-03-01 16:40:21 -0500

    basic functionality

--- script.js
+++ script.js
@@ -1,23 +1,72 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  let ciders = document.querySelectorAll('.ciders')
-  let ciderdisc = document.querySelectorAll('.ciderdisc')
+  const ciders = document.querySelectorAll('.ciders')
+  const ciderdisc = document.querySelectorAll('.ciderdisc')
+  const coverimage = document.querySelector('.coverpage')
+  const secondimage = document.querySelector('.page2')
+  const afterharvest = document.querySelector('#title')
+  const logo = document.querySelector('#logo')
+  const navcontainer = document.querySelector('.navcontainer')
 
-  let selectCider = (e) => {
-    // ciders.forEach (cider => {cider.style.opacity = '0.5'})
-    ciders.forEach((cider) => {cider.style.backgroundColor = 'rgba(128, 128, 128, 0.8)'})
-    // e.target.style.opacity = "0"
-    e.target.style.backgroundColor = 'rgba(128, 128, 128, 0.1)'
-    ciderdisc.forEach((disc) => disc.style.visibility = 'hidden')
-    e.target.children[0].style.visibility = 'visible'
-    if (window.innerWidth/2 > e.target.offsetLeft) {
-      e.target.children[0].style.left = '50vw'
-    } else {
-      e.target.children[0].style.left = '5vw'
-    }
+  // this should be run inside a screen resize function
+  const logoposition = logo.offsetTop + logo.height
 
+  function debounce(func, wait = 10, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
   }
 
-  ciders.forEach((cider) => cider.addEventListener('click', selectCider))
+  function selectCider(e){
+    ciders.forEach((cider) => {cider.style.backgroundColor = 'rgba(128, 128, 128, 0.8)'})
+    e.target.style.backgroundColor = 'rgba(128, 128, 128, 0.0001)'
+    e.target.children[0].style.visibility = 'visible'
+    if (window.innerWidth/2 > e.target.offsetLeft) {
+      e.target.children[0].style.left = '55vw'
+    } else {
+      e.target.children[0].style.left = '10vw'
+    }
+  }
 
+  function removeCider(){
+    ciders.forEach((cider) => {cider.style.backgroundColor = 'rgba(128, 128, 128, 0.00001)'})
+    ciderdisc.forEach((disc) => disc.style.visibility = 'hidden')
+  }
+
+  function picturescroll() {
+    coverimage.style.top = `${window.scrollY/1.5}px`
+    if (window.scrollY >= `${(window.innerHeight * .4)}`) {
+      secondimage.style.top = `${window.scrollY/2.25 + (window.innerHeight*.82)}px`
+      secondimage.style.zIndex = "-3"
+    } else {
+      secondimage.style.zIndex = "-5"
+      secondimage.style.top = `${(window.innerHeight*.82)}px`
+    }
+    if ((window.scrollY + logoposition) >= afterharvest.offsetTop) {
+        navcontainer.style.backgroundColor = 'grey'
+        logo.style.height = `${(navcontainer.getBoundingClientRect().height * 3/4)}px`
+        logo.style.width = `${(navcontainer.getBoundingClientRect().height * 3/4)}px`
+        logo.style.top = `${(navcontainer.getBoundingClientRect().height * 1/8)}px`
+        logo.style.left = `${(navcontainer.getBoundingClientRect().height * 1/4)}px`
+    } else {
+      navcontainer.style.backgroundColor = ''
+      logo.style.height = '100px'
+      logo.style.width = '100px'
+      logo.style.top = '25px'
+      logo.style.left = '50px'
+    }
+  }
+  ciders.forEach((cider) => cider.addEventListener('mouseout', removeCider))
+  ciders.forEach((cider) => cider.addEventListener('mouseover', selectCider))
+  ciders.forEach((cider) => cider.addEventListener('click', selectCider))
+  window.addEventListener('scroll', debounce(picturescroll))
 })
